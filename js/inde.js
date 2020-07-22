e65c8ad2ff784725151c7810ec3ee0380f0af476
josuechevez <67529112+josuechevez@users.noreply.github.com>
2020-07-22 15:21:24 -0600

    Add files via upload

---
+++ js/inde.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    Swal.fire({
+        title: 'Reposteria Tupis',
+        text: 'Te informamos que nuestro servicios estan disponibles solo en territorio Salvadoreño.',
+        imageUrl: '../icon/pastelera.jpg',
+        imageWidth: 400,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+      })
+});
